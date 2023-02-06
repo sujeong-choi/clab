c5a2ee037d8e605a847bc8473e60ff8e7c2c4261
hiskiasD <back-end@swipenow.fr>
2023-02-06 17:14:54 +0900

    update android UI and restructure based on requirements

--- Android/app/src/main/java/com/example/android/VideoFragment.kt
+++ Android/app/src/main/java/com/example/android/VideoFragment.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.android.databinding.FragmentSecondBinding
+import com.example.android.databinding.VideoFragmentBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class VideoFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: VideoFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +23,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = VideoFragmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +31,9 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
