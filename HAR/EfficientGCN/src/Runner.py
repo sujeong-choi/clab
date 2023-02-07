30ab85660f08fd8ae877e359da36b7852046c7e7
weadione <224lsy@gmail.com>
2023-02-07 22:00:11 +0900

    Change Skeleton file generation code and keypoints
     - Change PoseModule to return only the upper half of the body,
       not the whole body
     - Change PoseModule to return keypoint of mediapipe,
       not keypoint of ntu basis
     - In the case of painting and interview data,
       data augmented by left and right inverting
     - Add codes to create skeleton files in main.py

--- HAR/EfficientGCN/src/Runner.py
+++ HAR/EfficientGCN/src/Runner.py
@@ -99,7 +99,7 @@ class Runner(Initializer):
         self.num_class = 38 #121
         
         for idx, (frames, _) in tqdm(enumerate(self.videoFiles)):
-            skeleton, __ = self.skeletonMaker.skeleton_inference(frames)
+            skeleton, _, _ = self.skeletonMaker.skeleton_inference(frames)
             
             skeleton_list = np.array([skeleton])
             skeleton_list = np.append(skeleton_list, zero_arr[:,:len(frames),:,:],axis=0)
