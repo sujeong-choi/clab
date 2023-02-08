d236d6ce81abdae8a724f93d376318472387e3c7
sujeong-choi <shalalastal@naver.com>
2023-02-09 00:11:54 +0900

    update processor.py and finetuning.py

--- HAR/EfficientGCN/src/finetuning.py
+++ HAR/EfficientGCN/src/finetuning.py
@@ -2,117 +2,14 @@ import logging, torch, numpy as np
 from tqdm import tqdm
 from time import time
 import torch.nn as nn
+from src.processor import Processor
 
 from . import utils as U
 from .initializer import Initializer
 
 action_names = ['painting','Staring','other thing']
 
-class FineTuning(Initializer):
-
-    def train(self, epoch):
-        self.model.train()
-        start_train_time = time()
-        num_top1, num_sample = 0, 0
-        train_iter = self.train_loader if self.no_progress_bar else tqdm(self.train_loader, dynamic_ncols=True)
-        for num, (x, y, _) in enumerate(train_iter):
-            self.optimizer.zero_grad()
-
-            # Using GPU
-            x = x.float().to(self.device)
-            y = y.long().to(self.device)
-
-            # Calculating Output
-            out, _ = self.model(x)
-
-            # Updating Weights
-            loss = self.loss_func(out, y)
-            loss.backward()
-            self.optimizer.step()
-            self.scheduler.step()
-            self.global_step += 1
-
-            # Calculating Recognition Accuracies
-            num_sample += x.size(0)
-            reco_top1 = out.max(1)[1]
-            num_top1 += reco_top1.eq(y).sum().item()
-
-            # Showing Progress
-            lr = self.optimizer.param_groups[0]['lr']
-            if self.scalar_writer:
-                self.scalar_writer.add_scalar('learning_rate', lr, self.global_step)
-                self.scalar_writer.add_scalar('train_loss', loss.item(), self.global_step)
-            if self.no_progress_bar:
-                logging.info('Epoch: {}/{}, Batch: {}/{}, Loss: {:.4f}, LR: {:.4f}'.format(
-                    epoch+1, self.max_epoch, num+1, len(self.train_loader), loss.item(), lr
-                ))
-            else:
-                train_iter.set_description('Loss: {:.4f}, LR: {:.4f}'.format(loss.item(), lr))
-
-        # Showing Train Results
-        train_acc = num_top1 / num_sample
-        if self.scalar_writer:
-            self.scalar_writer.add_scalar('train_acc', train_acc, self.global_step)
-        logging.info('Epoch: {}/{}, Training accuracy: {:d}/{:d}({:.2%}), Training time: {:.2f}s'.format(
-            epoch+1, self.max_epoch, num_top1, num_sample, train_acc, time()-start_train_time
-        ))
-        logging.info('')
-
-    def eval(self):
-        self.model.eval()
-        start_eval_time = time()
-        with torch.no_grad():
-            num_top1, num_top3 = 0, 0
-            num_sample, eval_loss = 0, []
-            cm = np.zeros((self.num_class, self.num_class))
-            eval_iter = self.eval_loader if self.no_progress_bar else tqdm(self.eval_loader, dynamic_ncols=True)
-            for num, (x, y, _) in enumerate(eval_iter):
-
-                # Using GPU
-                x = x.float().to(self.device)
-                y = y.long().to(self.device)
-
-                # Calculating Output
-                out, _ = self.model(x)
-
-                # Getting Loss
-                loss = self.loss_func(out, y)
-                eval_loss.append(loss.item())
-
-                # Calculating Recognition Accuracies
-                num_sample += x.size(0)
-                reco_top1 = out.max(1)[1]
-                num_top1 += reco_top1.eq(y).sum().item()
-                reco_top3 = torch.topk(out,3)[1]
-                num_top3 += sum([y[n] in reco_top3[n,:] for n in range(x.size(0))])
-
-                # Calculating Confusion Matrix
-                for i in range(x.size(0)):
-                    cm[y[i], reco_top1[i]] += 1
-
-                # Showing Progress
-                if self.no_progress_bar and self.args.evaluate:
-                    logging.info('Batch: {}/{}'.format(num+1, len(self.eval_loader)))
-
-        # Showing Evaluating Results
-        acc_top1 = num_top1 / num_sample
-        acc_top3 = num_top3 / num_sample
-        eval_loss = sum(eval_loss) / len(eval_loss)
-        eval_time = time() - start_eval_time
-        eval_speed = len(self.eval_loader) * self.eval_batch_size / eval_time / len(self.args.gpus)
-        logging.info('Top-1 accuracy: {:d}/{:d}({:.2%}), Top-3 accuracy: {:d}/{:d}({:.2%}), Mean loss:{:.4f}'.format(
-            num_top1, num_sample, acc_top1, num_top3, num_sample, acc_top3, eval_loss
-        ))
-        logging.info('Evaluating time: {:.2f}s, Speed: {:.2f} sequnces/(second*GPU)'.format(
-            eval_time, eval_speed
-        ))
-        logging.info('')
-        if self.scalar_writer:
-            self.scalar_writer.add_scalar('eval_acc', acc_top1, self.global_step)
-            self.scalar_writer.add_scalar('eval_loss', eval_loss, self.global_step)
-
-        torch.cuda.empty_cache()
-        return acc_top1, acc_top3, cm
+class FineTuning(Processor):
 
     def start(self, num_class):
         start_time = time()
@@ -189,42 +86,3 @@ class FineTuning(Initializer):
                 logging.info('')
             logging.info('Finish training!')
             logging.info('')
-
-    def extract(self):
-        logging.info('Starting extracting ...')
-        if self.args.debug:
-            logging.warning('Warning: Using debug setting now!')
-            logging.info('')
-
-        # Loading Model
-        logging.info('Loading evaluating model ...')
-        checkpoint = U.load_checkpoint(self.args.work_dir, self.model_name)
-        if checkpoint:
-            self.cm = checkpoint['best_state']['cm']
-            self.model.module.load_state_dict(checkpoint['model'])
-        logging.info('Successful!')
-        logging.info('')
-
-        # Loading Data
-        x, y, names = iter(self.eval_loader).next()
-        location = self.location_loader.load(names) if self.location_loader else []
-
-        # Calculating Output
-        self.model.eval()
-        out, feature = self.model(x.float().to(self.device))
-
-        # Processing Data
-        data, label = x.numpy(), y.numpy()
-        out = torch.nn.functional.softmax(out, dim=1).detach().cpu().numpy()
-        weight = self.model.module.classifier.fc.weight.squeeze().detach().cpu().numpy()
-        feature = feature.detach().cpu().numpy()
-
-        # Saving Data
-        if not self.args.debug:
-            U.create_folder('./visualization')
-            np.savez('./visualization/extraction_{}.npz'.format(self.args.config),
-                data=data, label=label, name=names, out=out, cm=self.cm,
-                feature=feature, weight=weight, location=location
-            )
-        logging.info('Finish extracting!')
-        logging.info('')
