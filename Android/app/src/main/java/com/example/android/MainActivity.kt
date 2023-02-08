0ee56b77926c2a11aec11b6795c7358489d942ce
hiskiasD <back-end@swipenow.fr>
2023-02-08 12:50:44 +0900

    update UI and navigation between video and timelapse fragment

--- Android/app/src/main/java/com/example/android/MainActivity.kt
+++ Android/app/src/main/java/com/example/android/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -27,28 +24,5 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
--- Android/app/src/main/java/com/example/android/TimelapseFragment.kt
+++ Android/app/src/main/java/com/example/android/TimelapseFragment.kt
@@ -12,19 +12,14 @@ import com.example.android.databinding.TimelapseFragmentBinding
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class TimelapseFragment : Fragment() {
-
-    private var _binding: TimelapseFragmentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    lateinit var binding: TimelapseFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = TimelapseFragmentBinding.inflate(inflater, container, false)
+        binding = TimelapseFragmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,13 +27,8 @@ class TimelapseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.prevButton.setOnClickListener{
+            findNavController().navigate(R.id.action_TimelapseFragment_to_VideoFragment)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
