db5cd7b26d592301dbd92adac01143ceaa466ce1
hisku <hiskias.melke1@gmail.com>
2023-02-13 11:51:39 +0900

    update android to use onnx mobile deployment

--- Android/app/src/main/java/com/example/android/MainActivity.kt
+++ Android/app/src/main/java/com/example/android/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.android
 
+import ai.onnxruntime.OnnxTensor
+import ai.onnxruntime.OrtEnvironment
+import ai.onnxruntime.OrtSession
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Size
 import android.widget.VideoView
 import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.camera2.Camera2Config
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.CameraXConfig
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
@@ -20,6 +22,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.android.databinding.ActivityMainBinding
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.nio.FloatBuffer
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity(), CameraXConfig.Provider {
 
@@ -31,6 +38,7 @@ class MainActivity : AppCompatActivity(), CameraXConfig.Provider {
 
     private val REQUEST_CODE_PERMISSION = 101
     private val REQUIRED_PERMISSIONS: Array<String> = arrayOf("android.permission.CAMERA")
+    private val executor: Executor = Executors.newSingleThreadExecutor()
 
     override fun getCameraXConfig(): CameraXConfig {
         return Camera2Config.defaultConfig()
@@ -65,8 +73,8 @@ class MainActivity : AppCompatActivity(), CameraXConfig.Provider {
     }
 
     private fun checkPermissions():Boolean {
-        for (str in REQUIRED_PERMISSIONS) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
                 != PackageManager.PERMISSION_GRANTED
             ) {
                 return false
@@ -75,15 +83,61 @@ class MainActivity : AppCompatActivity(), CameraXConfig.Provider {
         return true
     }
 
+    // Create an OrtSession with the given OrtEnvironment
+    private fun createORTSession(ortEnvironment: OrtEnvironment ) : OrtSession {
+        val inputStream: InputStream = resources.openRawResource(R.raw.keypoint_rcnn)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val buffer = ByteArray(1024)
+        var len = inputStream.read(buffer)
+        while (len != -1) {
+            byteArrayOutputStream.write(buffer, 0, len)
+            len = inputStream.read(buffer)
+        }
+        val modelBytes = byteArrayOutputStream.toByteArray()
+        inputStream.close()
+        return ortEnvironment.createSession( modelBytes )
+    }
+
+    // Make predictions with given inputs
+    private fun runPrediction( input : ImageProxy , ortSession: OrtSession , ortEnvironment: OrtEnvironment ) : Float {
+        // Get the name of the input node
+        val inputName = ortSession.inputNames?.iterator()?.next()
+        // Make a FloatBuffer of the inputs
+        val floatBufferInputs = FloatBuffer.wrap( floatArrayOf( input ) )
+        // Create input tensor with floatBufferInputs of shape ( 1 , 1 )
+        val inputTensor = OnnxTensor.createTensor( ortEnvironment , floatBufferInputs , longArrayOf( 1, 1 ) )
+        // Run the model
+//        val results = ortSession.run( mapOf( inputName to inputTensor ) )
+//        // Fetch and return the results
+//        val output = results[0].value as Array<FloatArray>
+//        return output[0][0]
+        return 0.1F
+    }
+
+
+
     private fun startCamera(@NonNull cameraProvider: ProcessCameraProvider ) {
         // Set up the preview use case to display camera preview.
         val preview = Preview.Builder().build()
 
         val cameraSelector: CameraSelector = CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-
         preview.setSurfaceProvider(videoView.surfaceProvider)
 
-        val camera: Camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder().setTargetResolution(Size(1280, 720)).build()
+
+        val ortEnvironment = OrtEnvironment.getEnvironment()
+        val ortSession = createORTSession( ortEnvironment )
+
+        imageAnalysis.setAnalyzer(executor, ImageAnalysis.Analyzer { imageProxy ->
+            val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+            // insert your code here.
+            // after done, release the ImageProxy object
+            val output = runPrediction( imageProxy. , ortSession , ortEnvironment )
+
+            imageProxy.close()
+        })
+
+        val camera: Camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
     }
 }
